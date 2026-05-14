6244ee2698b7a17221e9f4df3c1c87f9c82ff813
Renato Gonzales <48256361+rgonzs@users.noreply.github.com>
2026-05-14 18:51:04 -0500

    add 1password

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,5 +5,6 @@ dnf5 install -y neovim
 dnf5 copr enable -y atim/starship
 dnf5 install -y starship
 dnf5 install -y @virtualization
-curl -L https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm  -o /tmp/1password.rpm
-rpm -i /tmp/1password.rpm
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo
+dnf5 install -y 1password
